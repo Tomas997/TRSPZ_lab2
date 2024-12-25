4f172aac51c80e2e01fb767c3528da982bd01b29
Andriy Plaksenkov <plaksenkov.andriy@lll.kpi.ua>
2024-12-25 15:06:20 +0200

    added migration

--- src/main/resources/db/migration/V1_0__init_tables.sql
+++ src/main/resources/db/migration/V1_0__init_tables.sql
@@ -1,9 +1,10 @@
-
 CREATE TABLE users
 (
-    id   SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL
+    id       SERIAL PRIMARY KEY,
+    username VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
 );
+
 
 CREATE TABLE categories
 (
@@ -11,18 +12,23 @@ CREATE TABLE categories
     name VARCHAR(100) NOT NULL
 );
 
+
 CREATE TABLE records
 (
     id                   SERIAL PRIMARY KEY,
     user_id              INT              NOT NULL,
     category_id          INT              NOT NULL,
     time_creation_record TIMESTAMP        NOT NULL,
-    sum_of_spent         DOUBLE PRECISION NOT NULL
+    sum_of_spent         DOUBLE PRECISION NOT NULL,
+    CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users (id),
+    CONSTRAINT fk_category FOREIGN KEY (category_id) REFERENCES categories (id)
 );
+
 
 CREATE TABLE accounts
 (
     id      SERIAL PRIMARY KEY,
     user_id INT              NOT NULL,
-    balance DOUBLE PRECISION NOT NULL
+    balance DOUBLE PRECISION NOT NULL,
+    CONSTRAINT fk_user_account FOREIGN KEY (user_id) REFERENCES users (id)
 );
--- src/main/resources/db/migration/V1_1__populate.sql
+++ src/main/resources/db/migration/V1_1__populate.sql
@@ -1,22 +1,28 @@
 
-INSERT INTO users (name)
-VALUES ('Alice'),
-       ('Bob'),
-       ('Charlie');
+
+INSERT INTO users (username, password)
+VALUES
+    ('Alice', '$2a$10$uQwtb4rEhUV2Nnq7ippUT.fWaqX1cB1rfjeVZWxHtWnPHDRAAvtQ.'),      --password
+    ('Bob', '$2a$10$b8U7sz.tikU1tfWq81SGCeNTpH1qZc/7Cg.F2SugyhVBW1AMJSsHm'),        --password2
+    ('Charlie', '$2a$10$wJ879Wti8h4xv36RINCJmuMogmMoSKYh38xTR4wSbGBzcNoG0xLMu');    --password3
+
 
 INSERT INTO categories (name)
-VALUES ('Groceries'),
-       ('Transport'),
-       ('Utilities');
+VALUES
+    ('Groceries'),
+    ('Transport'),
+    ('Utilities');
 
 
 INSERT INTO accounts (user_id, balance)
-VALUES (1, 100.50),
-       (2, 250.75),
-       (3, 0.00);
+VALUES
+    (1, 100.50),
+    (2, 250.75),
+    (3, 0.00);
 
 
 INSERT INTO records (user_id, category_id, time_creation_record, sum_of_spent)
-VALUES (1, 1, '2024-12-01 10:00:00', 50.00),
-       (2, 2, '2024-12-05 14:30:00', 20.00),
-       (3, 3, '2024-12-10 09:15:00', 100.00);
+VALUES
+    (1, 1, '2024-12-01 10:00:00', 50.00),
+    (2, 2, '2024-12-05 14:30:00', 20.00),
+    (3, 3, '2024-12-10 09:15:00', 100.00);
